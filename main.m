f20cf97b0524a3a2a040aba0b0c3e1d92ee08abd
Joachim Bean <jtb@me.com>
2009-05-11 23:34:47 -0400

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Am I Connected
+//
+//  Created by Joachim Bean on 4/13/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
