770f00fce7ea19dfd160ea448c27850eda17632d
Joachim Bean <jtb@me.com>
2009-05-11 23:35:45 -0400

    first commit

---
+++ WebViewController.h
@@ -0,0 +1,16 @@
+//
+//  WebViewController.h
+//  Am I Connected? 
+//
+//  Created by Joachim Bean on 4/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WebViewController : UIViewController {
+	IBOutlet UIWebView *webView;
+}
+
+@end
