34196419b095c592a09a1b53181449daceddb1b3
Joachim Bean <jtb@me.com>
2009-05-11 23:36:03 -0400

    first commit

---
+++ WebViewController.m
@@ -0,0 +1,63 @@
+//
+//  WebViewController.m
+//  Am I Connected? 
+//
+//  Created by Joachim Bean on 4/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+
+@implementation WebViewController
+
+
+// viewWillAppear loads every time you open up this View
+- (void)viewWillAppear:(BOOL)animated {
+	NSURL *theURL = [NSURL URLWithString:@"http://www.apple.com"];
+	[webView loadRequest:[NSURLRequest requestWithURL:theURL]]; 	
+}
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
